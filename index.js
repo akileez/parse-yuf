a5d3d8ce437a4a47c5c5f8f4c3df9abf482553db
akileez <git@keeda.net>
2015-09-16 20:08:52 -0400

    source code upload

--- index.js
+++ index.js
@@ -5,8 +5,33 @@
  * Licensed under the ISC license.
  */
 
-'use strict'
+var fs      = require('fs')
+var path    = require('path')
+var extract = require('yufrontin')
 
-module.exports = function () {
-  // do stuff
+function parse (input, cb) {
+  fs.readFile(input, 'utf8', function (err, res) {
+    if (err) return cb(err, res)
+
+    var fm = extract(res)
+
+    cb(null, {
+      abs     : fs.realpathSync(input),
+      rel     : input,
+      file    : path.parse(input),
+      stats   : fs.lstatSync(input),
+      data    : fm.data,
+      content : fm.content
+    })
+  })
 }
+
+function parseSync (input) {
+  var str = fs.readFileSync(input, 'utf8')
+  var fm = extract(str)
+
+  return fm
+}
+
+module.exports = parse
+module.exports.sync = parseSync
